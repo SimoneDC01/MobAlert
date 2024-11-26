6cd9eb5d0b733ee21e2fb3e52533cf7b0892fd60
Simone Di Cocco <simonedc01@gmail.com>
2024-11-26 16:08:40 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mobalert/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mobalert
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobalert.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            Toast.makeText(this, "Login button clicked", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
