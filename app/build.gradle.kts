bf0eaf52b571d01805b279881e791df06c0ce0a3
andreaciprotti <131988167+andreaciprotti@users.noreply.github.com>
2024-12-03 17:18:57 +0100

    delete account

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,8 @@ dependencies {
     implementation (libs.androidx.credentials)
     implementation (libs.play.services.auth.v1810)
     implementation (libs.glide)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
 }
--- app/src/main/java/com/example/mobalert/HomeFragment.kt
+++ app/src/main/java/com/example/mobalert/HomeFragment.kt
@@ -6,16 +6,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.mobalert.databinding.FragmentHomeBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
     private lateinit var referece: DatabaseReference
+
+    private val client = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+            })
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,16 +51,38 @@ class HomeFragment : Fragment() {
         database = FirebaseDatabase.getInstance()
         referece = database.reference.child("Users")
         binding.prova.setOnClickListener{
-            referece.child(auth.uid.toString()).setValue("prova").addOnSuccessListener {
-                Log.d("LOGIN", "prova: OK")
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    getAlerts()
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(context, "Alert inserito con successo!", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(context, "Errore: ${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
-            .addOnFailureListener {e->
-                Log.d("LOGIN", "prova: ERROR $e")
-            }
-            Log.d("LOGIN", "prova: ${referece}")
+
         }
 
         return binding.root
     }
+
+    private suspend fun getAlerts() {
+        val url = "https://deep-jaybird-exotic.ngrok-free.app/alerts"
+        try {
+            val response: HttpResponse = client.get(url)
+            if (response.status == HttpStatusCode.OK) {
+
+                Log.d("LOGIN", "alerts: ${response.bodyAsText()}")
+            } else {
+                Log.e("LOGIN", "Errore nella richiesta: ${response.status}")
+            }
+        } catch (e: Exception) {
+            Log.e("LOGIN", "Errore durante la richiesta: $e")
+        }
+    }
+
 
 }
